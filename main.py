5d2f94d815aa897d426f44866ded26c679e63fc4
Saravanan R <saravananr95m16@gmail.com>
2021-02-21 16:00:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# 0:00
+import turtle
+a = 5 
+print(a)
+
+b = "hello"
+print(b)
+
+# This is a square 
+# sar_turtle = turtle.Turtle()
+# sar_turtle.forward(100)
+# sar_turtle.right(90)
+# sar_turtle.forward(100)
+# sar_turtle.right(90)
+# sar_turtle.forward(100)
+# sar_turtle.right(90)
+# sar_turtle.forward(100)
+# sar_turtle.right(90)
+
+# 9:18
+sar_turtle = turtle.Turtle()
+
+def square():  
+  sar_turtle.forward(100)
+  sar_turtle.right(90)
+  sar_turtle.forward(100)
+  sar_turtle.right(90)
+  sar_turtle.forward(100)
+  sar_turtle.right(90)
+  sar_turtle.forward(100)
+
+square()
+sar_turtle.forward(200)
+square()
+
+#13:46
+
+# Calenda
